19712bf691fa3daaa4742f1e414275f31a54811c
Gabriele Tramonte <gabry.t92@gmail.com>
2021-11-29 11:32:39 +0100

    generated elements are shuffled

--- src/bin_generator.cpp
+++ src/bin_generator.cpp
@@ -22,10 +22,21 @@ int main(int argc, char* argv[]) {
     std::cout<<"Generating "<<n<<" elements with uniform distribution"<<std::endl;
     a = (data_t *) malloc(n * sizeof(data_t));
     for (i = 0; i < n; i++) {
-        a[i] = (double) (i) / (double) (n);
+        a[i] = (float) (i) / (float) (n);
         //a[i] = (data_t) i *  ((data_t) std::rand() /RAND_MAX)/ (data_t) n;
         //std::cout << a[i] << std::endl;
     }
+
+    for (i = 0; i < n; i++)
+    {
+        //size_t j = i + rand() / (RAND_MAX / (len - i) + 1);
+        int j = i +(rand()%(n-i));
+        float t = a[j];
+        a[j] = a[i];
+        a[i] = t;
+        //fprintf(stdout,"%d ",array[i]);
+    }
+
     //fout.open("streamdata");
     fout.write((char *) &n, sizeof(long));
     fout.write((char *) a, sizeof(data_t) * n);
