c2ffb122c041b3935243a755070ab9c5f03960e6
Gabriele Tramonte <gabry.t92@gmail.com>
2021-12-08 23:41:26 +0100

    benchmark req_parallel_v4

--- benchmark.sh
+++ benchmark.sh
@@ -2,6 +2,6 @@
 
 for p in {1..8}
 do
-  OUTPUT=$(mpirun --np $p ./build/req_parallel_v2 ./build/vector_float_1G.dat | grep "ParallelREQ")
+  OUTPUT=$(mpirun --np $p ./build/req_parallel_v4 ./build/vector_float_1G.dat ./build/ground_truth_1G.dat 0 | grep "ParallelREQ")
   echo $OUTPUT
 done
