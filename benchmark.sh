0960b159006f4961e0093f63c4c92694405427af
Gabriele Tramonte <gabry.t92@gmail.com>
2022-01-03 18:53:41 +0100

    benchmark with 4 core

--- benchmark.sh
+++ benchmark.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for p in {1..8}
+for p in {1..4}
 do
   OUTPUT=$(mpirun --np $p ./build/req_parallel_v4 ./build/vector_float_1G.dat ./build/ground_truth_1G.dat 0 | grep "ParallelREQ")
   echo $OUTPUT
